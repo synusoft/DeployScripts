39ff26d991e4b8991aa1bb2b7d62cd61a71ee274
EdwardYin <superyuri@yeah.net>
2017-06-01 14:36:15 +0800

    Update Portmodify.ps1

--- Portmodify.ps1
+++ Portmodify.ps1
@@ -1,10 +1,5 @@
-# 此脚本是帮助开启虚拟机防火墙的某一个端口,未完成
-# PortNo是端口号
-$PortNo = "3306"
-
-
-
-New-NetFirewallRule -Name %PortNo-tcp -Direction Inbound -DisplayName 'PowerShell远程连接 TCP' -LocalPort 5985-5996 -Protocol 'TCP'
-
-
-New-NetFirewallRule -Name powershell-remote-tcp -Direction Outbound -DisplayName 'PowerShell远程连接 TCP' -LocalPort 5985-5996 -Protocol 'TCP'
+Write-Host 此脚本的功能是帮助开启虚拟机防火墙的某一个端口
+$PortNo = Read-Host 输入要开启的端口号
+$DisplayName = Read-Host 输入协议名称
+New-NetFirewallRule -Name $DisplayName-$PortNo-tcp-in -Direction Inbound -DisplayName $DisplayName -LocalPort $PortNo -Protocol 'TCP'
+New-NetFirewallRule -Name $DisplayName-$PortNo-tcp-out -Direction Outbound -DisplayName $DisplayName -LocalPort $PortNo -Protocol 'TCP'
